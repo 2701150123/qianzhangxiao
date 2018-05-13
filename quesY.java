f5e13feb01ffded7003fad8b158ed1ffbde7e214
2701150123 <37103376+2701150123@users.noreply.github.com>
2018-05-13 19:50:55 +0800

    Create quesY.java

---
+++ quesY.java
@@ -0,0 +1,18 @@
+# qianzhangxiao
+package xin;
+
+public class Q {
+
+	public static void main(String[] args) {
+		double s;
+for(s=1;;s++) {
+	if((s-60*(2*s-30)/(s+30))==60)
+		
+	System.out.printf("%.4f",s);
+	   }
+	}
+	
+		
+	}
+
+
